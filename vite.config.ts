7d33ad6e7fe36cb8c83328c3b0f0c827711d34a9
iftakharul28 <iftakharul28@gmail.com>
2023-11-07 21:37:04 +0600

    build file changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   emptyOutDir: true,
-  //   outDir: '../../docs',
-  // },
 });
